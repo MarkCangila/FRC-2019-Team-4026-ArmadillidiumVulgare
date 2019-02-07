bada58d7ba35b6b2d414515937ba9f2bd22b348b
ABoredIdiot <80bertbeel@csdecatur.net>
2019-02-07 16:18:56 -0500

    initial commit

---
+++ src/main/java/frc/robot/Portmap.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+
+public class Portmap{
+    //Drive Motors
+    public static final int LEFTDRIVE = 0;
+    public static final int RIGHTDRIVE = 1;
+    
+    public static final int HATCHGRABBER = 5;
+    //Flipper Motors
+    public static final int LEFTFLIPPER = 3;
+    public static final int RIGHTFLIPPER = 4;
+}
---
+++ src/main/java/frc/robot/commands/DriveTrainCMDS.java
@@ -0,0 +1,45 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class DriveTrainCMDS {
+  public static class StraightDrive extends Command{
+    double power;
+    public StraightDrive(){
+    }
+
+    protected void initialize(){}
+
+    protected void execute(){
+      Robot.driveTrain.leftPower(Robot.oi.stick.getThrottle());
+      Robot.driveTrain.rightPower(Robot.oi.stick.getThrottle());
+    }
+
+    protected boolean isFinished(){
+      return false;
+    }
+  }
+  public static class TankDrive extends Command {
+
+    double rightPower, leftPower;
+
+    public TankDrive() {
+      // Use requires() here to declare subsystem dependencies
+      requires(Robot.driveTrain);
+    }
+
+    protected void initialize() {}
+
+    @Override
+    protected void execute() {
+      Robot.driveTrain.leftPower(Robot.oi.stick.getThrottle());
+      Robot.driveTrain.rightPower(Robot.oi.stick.getY());
+    }
+
+    @Override
+    protected boolean isFinished() {
+      return false;
+    }
+  }
+}
