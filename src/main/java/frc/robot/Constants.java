06d90dfa9afeb11f39259f345ff48f4b00186da7
Mark <keonsanavandijoon@gmail.com>
2021-02-03 13:57:55 -0500

    Add constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,12 +4,12 @@ import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
 
 public class Constants {
     // TODO: Set all of these using characterization tool
-    public static final double ksVolts = 0;
-    public static final double kvVoltSecondsPerMeter = 0;
-    public static final double kaVoltSecondsSquaredPerMeter = 0;
-    public static final double kPDriveVel = 0;
+    public static final double ksVolts = 1.21;
+    public static final double kvVoltSecondsPerMeter = 3.32;
+    public static final double kaVoltSecondsSquaredPerMeter = 0.0438;
+    public static final double kPDriveVel = 0.714;
     // TODO: Measure the horizontal distance between wheels and set trackwidth
-    public static final double kTrackwidthMeters = 0.6096;
+    public static final double kTrackwidthMeters = 1.3025332494619235;
     public static final DifferentialDriveKinematics kDriveKinematics =
         new DifferentialDriveKinematics(kTrackwidthMeters);
     // TODO: We should set this - I'm leaving it at 3 for now bc I know our robot is capable of it
